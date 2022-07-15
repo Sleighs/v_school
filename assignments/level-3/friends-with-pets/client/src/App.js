5ed2c7ccec4d979ffb8f936c9415fe6a008db32c
Samuel <wsright987@gmail.com>
2022-07-15 00:02:32 -0400

    friends with pets

--- assignments/level-3/friends-with-pets/client/src/App.js
+++ assignments/level-3/friends-with-pets/client/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import friends from './friends'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      marginTop: '2%',
+    }}>
+      <div style={{
+        color: 'white',
+        fontSize: '3em',
+        textAlign: 'center',
+        margin: '3% auto',
+        fontFamily: 'Monaco',
+      }}>Friends With Pets</div>
+      <FriendList />
     </div>
   );
+}
+
+const FriendList = props => {
+  return (
+    <div className="friend-list__container">
+      {friends.map((item, i) => 
+        <Friend 
+          name={item.name}
+          age={item.age}
+          pets={item.pets}
+          key={i}
+        />
+      )}
+    </div>
+  )
+}
+
+const Friend = props => {
+  const {
+    name,
+    age,
+    pets,
+    key
+  } = props
+
+  return (
+    <div className="friend__container">
+      <div>
+        <div className="friend__name">{name}</div>
+        <div className="friend__age">{age}</div>
+      </div> 
+      <div className="friend__pets">
+        {pets.map((item, i) => 
+          <Pet 
+            name={item.name} 
+            breed={item.breed} 
+            key={i}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+const Pet = props => {
+  const { name, breed, key} = props
+
+  return (
+    <div className="pet__container">
+      <div className="pet__name">{name}</div>
+      <div className="pet__breed">{breed}</div>
+    </div>
+  )
 }
 
 export default App;
---
+++ assignments/level-3/friends-with-pets/client/src/friends.js
@@ -0,0 +1,59 @@
+const friends = [
+    {
+      name: "Ben",
+      age: 29,
+      pets: [
+        {
+          name: "Spot",
+          breed: "Tabby"
+        },{
+          name: "John Johnson",
+          breed: "husky"
+        },{
+          name: "Bear the bear",
+          breed: "Grizzly"
+        }
+      ]
+    },{
+      name: "Bob",
+      age: 31,
+      pets: [
+        {
+          name: "Sally",
+          breed: "Australian Shepard"
+        }
+      ]
+    },{
+      name: "Marcus",
+      age: 25,
+      pets: [
+        {
+          name: "Indy",
+          breed: "Akita"
+        },{
+          name: "Anna",
+          breed: "Persian cat"
+        }
+      ]
+    },{
+      name: "Jacob",
+      age: 20,
+      pets: [
+        {
+          name: "Fluffy",
+          breed: "Sphynx cat"
+        },{
+          name: "Patches",
+          breed: "Sphynx cat"
+        },{
+          name: "Tiger",
+          breed: "Sphynx cat"
+        },{
+          name: "Oscar",
+          breed: "Sphynx cat"
+        }
+      ]
+    }
+  ]
+
+  export default friends
